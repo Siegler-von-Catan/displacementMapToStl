5b7960ab9bc09477c79bdc71d11fc254aabfb66f
Joana Be <33000454+JoeAtHPI@users.noreply.github.com>
2021-01-19 20:42:12 +0100

    Update displacementMapToStl.py

--- src/displacementMapToStl.py
+++ src/displacementMapToStl.py
@@ -61,7 +61,7 @@ bpy.context.object.modifiers["CorrectiveSmooth"].iterations = 15
 
 # finally decrease face countwith decimate modifier
 bpy.ops.object.modifier_add(type='DECIMATE')
-bpy.context.object.modifiers["Decimate"].ratio = 0.2
+bpy.context.object.modifiers["Decimate"].ratio = 0.015
 
 # boolean substract on the wax stamp model of 
 
